08a143e0332d125488fdc8306eb42b0ccfdecdcf
Steffen Fohr <sfohr@users.noreply.github.com>
2024-07-31 16:25:29 +0200

    calibrate temperature on start

--- 03_loveometer/src/main.cpp
+++ 03_loveometer/src/main.cpp
@@ -1,23 +1,59 @@
 #include <Arduino.h>
 
 const int sensorPin = A0;
-const float baselineTemp = 24.0;
+float baselineTemp = 20.0;
 const float deltaTemp = 2.0;
-const unsigned ledPins[] = {2, 3, 4, 5};
-const int nTempLEDs = sizeof(ledPins) / sizeof(int);
+const unsigned ledPinsTemp[] = {2, 3, 4, 5};
+const unsigned ledPinCalibrating = 8;
+const int nTempLEDs = sizeof(ledPinsTemp) / sizeof(int);
+int baselineTempSensorRaw = 0;
 
-float computeVoltage(int sensorVal);
-float computeTempFromVolt(float voltage);
+float computeVoltage(int sensor_value) {
+  // ADC value (0 to 1024) to voltage (0.0 to 5.0V)
+  return (sensor_value / 1024.0) * 5.0;
+}
 
-void updateTemperatureLEDs(float temperature, int nLEDs);
+float computeTempFromVolt(float voltage) {
+  // TMP36 scale factor: 10mV/°C
+  return (voltage - .5) * 100.0;
+}
+
+void updateTemperatureLEDs(float temperature, float baseline_temp, int nLEDs) {
+  int numLEDsOn = (temperature - baseline_temp) / deltaTemp;
+  numLEDsOn = constrain(numLEDsOn, 0, nLEDs);
+
+  for (int i = 0; i < nLEDs; i++) {
+    if (i < numLEDsOn) {
+      digitalWrite(ledPinsTemp[i], HIGH);
+    } else {
+      digitalWrite(ledPinsTemp[i], LOW);
+    }
+  }
+}
 
 void setup() {
   Serial.begin(9600);
 
   for (int i = 0; i < nTempLEDs; i++) {
-    pinMode(ledPins[i], OUTPUT);
-    digitalWrite(ledPins[i], LOW);
+    pinMode(ledPinsTemp[i], OUTPUT);
+    digitalWrite(ledPinsTemp[i], LOW);
   }
+  pinMode(ledPinCalibrating, OUTPUT);
+  digitalWrite(ledPinCalibrating, HIGH);
+  int i = 0;
+  float baselineVoltageSum = 0.0;
+  int sensorVal = 0;
+  while (millis() < 1000) {
+    sensorVal = analogRead(sensorPin);
+    baselineVoltageSum += computeVoltage(sensorVal);
+    i += 1;
+    delay(1);
+  }
+  float baselineVoltage = baselineVoltageSum / i;
+  baselineTemp = computeTempFromVolt(baselineVoltage);
+  Serial.print("Baseline temperatur (degree celsius): ");
+  Serial.println(baselineTemp);
+  digitalWrite(ledPinCalibrating, LOW);
 }
 
 void loop() {
@@ -33,29 +69,6 @@ void loop() {
   Serial.print(", Degree C: ");
   Serial.println(temperature);
 
-  updateTemperatureLEDs(temperature, nTempLEDs);
+  updateTemperatureLEDs(temperature, baselineTemp, nTempLEDs);
   delay(1);
-}
-
-float computeVoltage(int sensor_value) {
-  // ADC value (0 to 1024) to voltage (0.0 to 5.0V)
-  return (sensor_value / 1024.0) * 5.0;
-}
-
-float computeTempFromVolt(float voltage) {
-  // TMP36 scale factor: 10mV/°C
-  return (voltage - .5) * 100.0;
-}
-
-void updateTemperatureLEDs(float temperature, int nLEDs) {
-  int numLEDsOn = (temperature - baselineTemp) / deltaTemp;
-  numLEDsOn = constrain(numLEDsOn, 0, nLEDs);
-
-  for (int i = 0; i < nLEDs; i++) {
-    if (i < numLEDsOn) {
-      digitalWrite(ledPins[i], HIGH);
-    } else {
-      digitalWrite(ledPins[i], LOW);
-    }
-  }
 }
