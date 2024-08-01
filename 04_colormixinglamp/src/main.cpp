1927b8f210d51b2cf8515944927c459cab64a54a
Steffen Fohr <sfohr@users.noreply.github.com>
2024-08-01 21:23:03 +0200

    adjust pins, comments

--- 04_colormixinglamp/src/main.cpp
+++ 04_colormixinglamp/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 
 const int greenLEDPin = 9;
-const int redLEDPin = 10;
-const int blueLEDPin = 11;
+const int redLEDPin = 11;
+const int blueLEDPin = 10;
 
 const int redSensorPin = A0;
 const int greenSensorPin = A1;
@@ -34,25 +34,33 @@ void loop() {
   blueSensorValue = analogRead(blueSensorPin);
   delay(5);
 
-  Serial.print("Raw data from sensor \t red: ");
+  Serial.println("Raw data from sensor:");
+  Serial.print("\t red: ");
   Serial.print(redSensorValue);
   Serial.print("\t green: ");
   Serial.print(greenSensorValue);
   Serial.print("\t blue: ");
-  Serial.print(blueSensorValue);
+  Serial.println(blueSensorValue);
 
+  // Raw sensor values range from 0 to 1023, convert to RGB, 0 to 255
   redValue = redSensorValue/4;
   greenValue = blueSensorValue/4;
   blueValue = blueSensorValue/4;
 
-  Serial.print("Summary of sensor values: \t red: ");
+  Serial.println("Summary of sensor values:");
+  Serial.print("\t red: ");
   Serial.print(redValue);
   Serial.print("\t green: ");
   Serial.print(greenValue);
   Serial.print("\t blue: ");
-  Serial.print(blueValue);
+  Serial.println(blueValue);
 
+  // Digital pins output binary signals (0/1). To control LED brightness, we use Pulse Width
+  // Modulation (PWM), which adjusts the ratio of time the signal is 1 versus 0. This creates
+  // a perceived analog output, dimming the LEDs based on the duty cycle (percentage of time 
+  // the signal stays high during each cycle)
   analogWrite(redLEDPin, redValue);
   analogWrite(greenLEDPin, greenValue);
   analogWrite(blueLEDPin, blueValue);
+  delay(1000);
 }
